47d000d9921abbb9e22e9e4f4fb2526d299e7b33
AustinP <auphipps@csumb.edu>
2025-12-01 17:34:50 -0800

    added flask shel;

--- campsite_website.py
+++ campsite_website.py
@@ -1,1 +1,22 @@
-print (hello)
+import requests, json
+from flask import Flask, render_template, flash, redirect
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'otters2025'
+bootstrap = Bootstrap5(app)
+
+params = {
+
+}
+
+endpoint = "https://developer.nps.gov/api/v1/alerts?parkCode=acad,dena"
+
+try: 
+    r = requests.get(endpoint, params=params)
+    data = r.json()
+except:
+    print('please try again')
