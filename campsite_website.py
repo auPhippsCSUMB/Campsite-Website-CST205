a58cfab4f2756df5c902e532a7d5420e5f35c63c
alexisponce1 <aleponce@csumb.edu>
2025-12-03 17:33:28 -0800

    State filtering

--- campsite_website.py
+++ campsite_website.py
@@ -10,6 +10,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'otters2025'
 bootstrap = Bootstrap5(app)
 
+my_key = 'HDF4Pw02rLOOv9EuM4rU8LMgjIXs2O7FzxdfVEaX'
+
+payload = {
+    'api_key': my_key,
+    'q' : 'CA',
+    'limit' : 5
 
 # Configure API request
 endpoint = "https://developer.nps.gov/api/v1/alerts?"
@@ -17,12 +23,17 @@ HEADERS = {
     "X-Api-Key":"5Ejw0mGYYPhdABAjwaOzlDFtQQ7IyJik1DldHmuu"
     }
 
-# Additional code would follow
+endpoint = 'https://developer.nps.gov/api/v1/parks'
 
-try: 
-    req = requests.get(endpoint,headers=HEADERS)
-    data = req.json()
-    pprint(data)
+try:
+    r = requests.get(endpoint, params=payload)
+    data = r.json()
+    # pprint(data)
+    
+    for parks in data["data"]:
+        print (parks['fullName'], parks['states'])
+        
+
 except:
     print('please try again')
 
